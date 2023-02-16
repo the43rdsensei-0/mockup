74855182452ee64979a0053b75f3fc67df1de26d
the43rdsensei <ismailmuyideen.c05@gmail.com>
2023-02-16 08:25:05 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+const menuBtn = document.querySelector('.menu');
+const menuModal = document.querySelector('.menu-modal');
+const closeBtn = document.querySelector('.x-mark');
+const menuContent = document.querySelector('.menu-list')
+const container = document.querySelector('.container-wrapper')
+
+menuBtn.addEventListener('click', () => {
+    menuModal.classList.add('slide')
+    container.classList.add('blur');
+})
+
+closeBtn.addEventListener('click', () => {
+    menuModal.classList.remove('slide')
+    container.classList.remove('blur');
+})
+
+menuModal.addEventListener('click', () => {
+    menuModal.classList.remove('slide')
+    container.classList.remove('blur');
+})
+
+
+// interacting with the search-bar
+
+const searchBar = document.getElementById('search');
+const searchModal = document.querySelector('.search-modal');
+const esc = document.querySelector('.escape');
+
+searchBar.addEventListener('click', () => {
+    searchModal.classList.add('show-search')
+    container.classList.add('blur');
+});
+
+esc.addEventListener('click', () => {
+    searchModal.classList.remove('show-search')
+    container.classList.remove('blur');
+});
+
+searchModal.addEventListener('click', () => {
+    searchModal.classList.remove('show-search')
+    container.classList.remove('blur');
+});
+
+// carousel {the tough one sigh!}
+
+
+
---
+++ styleguide.md
@@ -0,0 +1,18 @@
+
+# typography
+
+font family : (https://fonts.google.com/specimen/Coda?preview.size=51) (font-family: 'Coda', cursive;)
+font size : 16px 
+
+# colors
+
+yellow : #f3aa41, hsl(35,88%,60%), rgb(243,170,65)
+green ; #7aac2e, hsl(84,58%,43%), rgb(122,172,46)
+white : #ffffff
+background (dark) : #1a1c1f, rgb(26,28,31), hsl(216,9%,11%)
+search input (dark grey) : #28292e, hsl(230,7%,17%), rgb(40,41,46)
+lighter grey : #3b3b41, hsl(240,5%,24%), rgb(59,59,65)
+
+# icons 
+
+(https://fontawesome.com/)
